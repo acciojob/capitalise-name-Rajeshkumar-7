722526b989c98ab4e160b94e4d9c2bdc365f6e60
Rajeshkumar-7 <83906427+Rajeshkumar-7@users.noreply.github.com>
2024-03-31 10:26:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function convertCase(){
+      let input = document.querySelector("#fname")
+      let val = input.value
+      input.value = val.toUpperCase()
+    }
